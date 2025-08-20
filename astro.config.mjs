724756b89847ddbd9b5e8fc632fec9b7f500fcda
alistermarc <alistermarcdomilies@gmail.com>
2025-08-20 23:09:47 +0800

    edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alistermarc.github.io',
-  base: '/astrofy/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
