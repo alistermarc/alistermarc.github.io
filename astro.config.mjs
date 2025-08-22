1a290de7e867bfb73db80e1aa6b9febe68f2e7ab
alistermarc <alistermarcdomilies@gmail.com>
2025-08-23 01:41:36 +0800

    edit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alistermarc.github.io',
+  // site: 'https://alistermarc.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
